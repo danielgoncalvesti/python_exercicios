bf3de412c0284986188d52c3d06bd703fe906b80
Daniel Gonçalves da Silva <danielgoncalvesti@gmail.com>
2024-04-06 18:59:23 -0300

    Update Lista04.py

--- exercicios-revisao/03-repetição/03-listas/Lista04.py
+++ exercicios-revisao/03-repetição/03-listas/Lista04.py
@@ -6,9 +6,3 @@ e exiba quantas vezes cada número aparece na lista.
 lista = [5,1,2,3,3,4,3,2,5]
 
 # comece aqui:
-for i in lista:
-    contador_ocorrencias = 0
-    for x in lista:
-        if i == x:
-            contador_ocorrencias += 1
-    print(f"O número {i} aparece {contador_ocorrencias} vezes na lista.")
